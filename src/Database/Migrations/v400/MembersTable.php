11c8217cd07237c192e61b608ce91295872b9ae7
alexweissman <alex@alexanderweissman.com>
2017-10-07 17:18:37 -0400

    simplify members table

--- src/Database/Migrations/v400/MembersTable.php
+++ src/Database/Migrations/v400/MembersTable.php
@@ -16,16 +16,13 @@ class MembersTable extends Migration
         if (!$this->schema->hasTable('members')) {
             $this->schema->create('members', function (Blueprint $table) {
                 $table->increments('id');
-                $table->integer('user_id')->unsigned()->unique();
                 $table->string('city', 255)->nullable();
                 $table->string('country', 255)->nullable();
-                $table->timestamps();
 
                 $table->engine = 'InnoDB';
                 $table->collation = 'utf8_unicode_ci';
                 $table->charset = 'utf8';
-                $table->foreign('user_id')->references('id')->on('users');
-                $table->index('user_id');
+                $table->foreign('id')->references('id')->on('users');
             });
         }
     }
